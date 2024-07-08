95845b22bc9c5d79a4da4314d4de7924022c0501
Shree Nivas Kushwah <shri.mca2010@gmail.com>
2024-07-09 00:11:07 +0530

    added files upload

--- src/main/java/com/example/EmployeeController.java
+++ src/main/java/com/example/EmployeeController.java
@@ -1,16 +1,19 @@
 package com.example;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
+    private static String UPLOAD_DIR = "uploads"; 
+
     @GetMapping("/hello/{name}")
     public String Hello(@PathVariable  String name ) {
         return "Hello you passed "+name;
@@ -18,5 +21,31 @@ public class EmployeeController {
     @PostMapping("/save")
     public String Save(@RequestBody Employee employee) {
         return "Posted Employee Obj";
+    }
+
+    @PostMapping("/uploadTextFile")
+    public String handleFileUpload(@RequestParam("files") List<MultipartFile> files) {
+
+        StringBuilder response = new StringBuilder("Files uploaded successfully:\n");
+
+
+         for (MultipartFile file : files) {
+            if (file.isEmpty()) {
+                continue; // Skip empty files
+            }
+            
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOAD_DIR + File.separator + file.getOriginalFilename());
+                Files.write(path, bytes); // Write file to local filesystem
+
+                response.append(file.getOriginalFilename()).append("\n");
+            } catch (IOException e) {
+                return "Failed to upload " + file.getOriginalFilename() + ": " + e.getMessage();
+            }
+        }
+
+        // Logic to handle file upload
+        return response.toString();
     }
 }
